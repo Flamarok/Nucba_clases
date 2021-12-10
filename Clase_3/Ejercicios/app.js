6bc3b394436ca44f53cab3da19245259ad8cfdca
Franco <flamarokprograming@gmail.com>
2021-12-09 21:34:31 -0300

    js

---
+++ Clase_3/Ejercicios/app.js
@@ -0,0 +1,90 @@
+// Ejercicio 1
+/*
+let texto = prompt ("Ingrese un texto");
+if(texto.indexOf('a') != -1 || texto.indexOf('A') != -1 ){
+alert("la letra a encontrada");
+}
+else {
+    alert ("la letra a no fue encontrada");
+}
+
+/*
+if(texto.includes("a"))
+{
+    alert("El texto contiene una a");
+}else
+{
+    alert("No contiene a");
+}
+*/
+
+
+// Ejercicio 2
+/*
+let texto = prompt ("Ingrese un texto");
+alert (texto.toLowerCase());
+alert (texto.toUpperCase());
+*/
+
+// Ejercicio 3
+/*
+let texto = prompt ("Ingrese un texto");
+alert(texto.slice(0,1));   // tira las letras desde donde empieza hasta donde se indique, en este caso la primera letra.
+*/
+
+// Ejercicio 4
+/*
+let texto = prompt ("Ingrese un texto");
+alert (texto.length);
+*/
+
+// Ejercicio 5
+/*
+let numero = prompt ("Ingrese un número");
+alert (numero.length);
+*/
+
+// Ejercicio 6
+/*
+let texto = prompt ("Ingrese un texto");
+let longitud = texto.length;
+alert (texto.charAt(longitud -1));
+*/
+
+// Ejercicio 7
+/*
+let texto = prompt ("Ingrese un texto");
+alert (texto.replaceAll(" ", "*"));
+*/
+
+// Ejercicio 8
+/*
+let texto = prompt ("Ingrese un texto");
+let letra = prompt ("Ingrese una letra");
+if(texto.indexOf(letra) != -1 ){
+    alert("la letra " + letra + " encontrada");
+    }
+    else {
+        alert ("la letra " + letra + " no fue encontrada");
+    }
+*/
+
+// Ejercicio 9 
+/*
+let texto = prompt ("Ingrese un texto");
+alert(texto.slice(1)) // para mostrar todo el texto menos la primera letra.
+*/
+
+// Ejercicio 10
+/*
+let nombre = prompt ("Ingrese su nombre en minúscula");
+alert (nombre.toUpperCase().charAt(0) + nombre.slice(1));
+*/
+
+
+// Ejercicios Condicionales (Parte 1)
+// Ejercicio 1
+let numero = prompt ("Ingrese un número");
+if (condition) {
+    
+}
--- Clase_3/app.js
+++ Clase_3/app.js
@@ -8,3 +8,43 @@ if(edad >= 18) {
 else {
     alert("No sos mayor de edad");
 }
+
+if(edad >= 18 && edad <= 21) {
+    alert("")
+}
+else if(edad > 21) {
+    alert("")
+}
+else {
+    alert("")
+}
+
+switch (new Date().getDay()){
+    case 0:
+        day = "Domingo";
+        alert(day);
+        break;
+    case 1:
+        day = "Lunes";
+        alert(day);
+        break;
+    case 2:
+        day = "Martes";
+        alert(day);
+        break;
+    case 3: 
+        day = "Miercoles";
+        alert(day);
+        break;
+    case 4:
+        day = "Jueves";
+        alert(day);
+        break;
+    case 5:
+        day = "Viernes";
+        alert(day);
+        break;
+    case 6:
+        day = "Sabado";
+        alert(day);
+}
