87c8266580f9d7b299d1a35077a8a4dbdeae3c6b
Franco <flamarokprograming@gmail.com>
2022-04-05 23:49:24 -0300

    Ejercicio y clase 6

---
+++ Ejercicios de JS/Proyecto entrega1.js
@@ -0,0 +1,23 @@
+
+
+var ingredientes = [
+  "masa",
+  "tomate",
+  "jamon",
+  "queso",
+  "chedar",
+  "oregano",
+  "sal",
+  "agua",
+  "azucar",
+  "aceite"
+];
+
+function pizzas(ingredientes) {
+  
+  const ingredientesPares = ingredientes.filter( ingredientes => (ingredientes.length % 2 == 0));
+  const ingredientesImpares = ingredientes.filter( ingredientes => (ingredientes.length % 2 != 0));
+  console.log("Los ingredientes de letras pares son:" + ingredientesPares + " Y los ingredientes de letras impares son: " + ingredientesImpares);
+}
+
+console.log(pizzas(ingredientes))
---
+++ clase_6_callbacks/index.js
@@ -0,0 +1,54 @@
+/*
+function mostrarResultado(resultado) {
+    console.log("El resultado es : ", resultado);
+}
+
+function calculadora(n1, n2, fn) {
+    let res = n1 + n2;
+    fn(res);
+}
+
+calculadora(5, 6, mostrarResultado);
+
+*/
+
+/*
+function saludar(nombre) {
+    console.log("Hola", nombre);
+}
+
+function procesarSaludo(callback) {
+    callback("Ariel");
+}
+
+procesarSaludo(saludar);
+*/
+
+/*
+function pato() {
+    console.log("yo soy un pato");
+}
+
+function gallina() {
+    console.log("yo soy una gallina");
+}
+
+function message(fn) {
+    fn();
+}
+
+message(gallina);
+*/
+
+/*
+function operaciones(a, b, callback) {
+  let resultado = a + b;
+  callback(resultado)
+}
+
+const resultados = (resultado) => {
+    console.log(resultado);
+}
+
+operaciones(1, 2, resultados);
+*/
