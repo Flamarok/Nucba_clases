2ca71ae645e32c7f696beb837ce948eede62d7e0
unknown <flamarokprograming@gmail.com>
2022-05-03 21:43:58 -0300

    clases

---
+++ Clase_10_Mod HTML y Eventos(Submit,Prevent,propagation)/Tarea/script.js
@@ -0,0 +1,8 @@
+const h1Principal = document.querySelector("body > div > h1");
+const parrafo = document.querySelector("body > p");
+
+document.addEventListener("DOMContentLoaded", () => {
+    h1Principal.style.color= "red";
+    parrafo.textContent=("Cambio de texto");
+});
+
