00fd2ed42b63bd90d982a7d498af57125bbc77aa
unknown <flamarokprograming@gmail.com>
2022-05-19 21:42:31 -0300

    clases

--- Clase_17_Async_Await(version nueva de fetch)/app2.js
+++ Clase_17_Async_Await(version nueva de fetch)/app2.js
@@ -1,17 +1,27 @@
+const valor = false
+
 function descargarCliente() {
     console.log('Descargando Cliente...')
-    return new Promise (resolve => {
-        setTimeout(()=> {
-            resolve('Cliente descargado')            
+    return new Promise ((resolve, reject) => {
+        setTimeout( ()=> {
+            if (valor) {
+                resolve('Cliente descargado')            
+            } else {
+                reject('El Cliente no se ha descargado')
+            }    
         }, 5000)
     })
 }
 
 function descargarProductos() {
     console.log('Descargando Productos...')
-    return new Promise (resolve => {
-        setTimeout(()=> {
-            resolve('Productos descargado')            
+    return new Promise ((resolve, reject) => {
+        setTimeout( ()=> {
+            if (!valor) {
+                resolve('Productos descargado')            
+            } else {
+                reject('Los Productos no se han descargado')
+            }
         }, 3000)
     })
 }
@@ -56,4 +66,27 @@ const app2 = async () => {
 
 // este ultimo al utilizar un solo await muestra todo al mismo tiempo, aunque una promesa deba esperar a la otra para mostrar su resultado. Devuelve todo al mismo tiempo
 
-/* Tarea agregar el reject en cada promesa y permutar los errores */
+/* Tarea agregar el reject en cada promesa y ejecutar*/
+
+const app3 = async () => {
+    try {
+        /* const respuesta = await Promise.all([descargarCliente(), descargarProductos()])
+        console.log(respuesta); */
+        // llama a las dos promesas la promesa que va al reject, bloquea la aparicion de la promesa resolv 
+        // hay que utilizar la siguiente manera
+        const respuesta = await Promise.allSettled([descargarClientes(), descargarProductos()])
+        mostrarHTML(respuesta)
+    } catch (error) {   
+        console.log(error)       
+    }
+}
+
+function mostrarHTML(respuesta) {
+    const body = document.querySelector('body')
+    respuesta.map(dato => {
+        // console.log(dato.value);
+        const title = document.createElement('h3')
+        title.textContent = dato.value
+        body.appendChild(title)
+    }) // hace un recorrido ya que las promesas son una array de objetos y necesitamos tomar el valor 
+}
--- Clase_17_Async_Await(version nueva de fetch)/app3.js
+++ Clase_17_Async_Await(version nueva de fetch)/app3.js
@@ -2,7 +2,7 @@ const url = "asdgdsgsdg"
 const body= document.querySelector("body")
 
 
-document.addEventListener("DOMContentLoaded", obtenerDatos)
+document.addEventListener("DOMContentLoaded", /* obtenerDatos o consultaAsync */ )
 
 
 
@@ -24,3 +24,61 @@ function descargarCliente() {
 } */
 
 // esta fetch se hace asi con async await
+
+const consultaAsync = async ()=>{
+    try {
+        const resultado = await fetch(url);
+        const resulJson = await resultado.json();
+        console.log(resulJson);
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const mostrarHTML = (respuesta) => {
+    respuesta.map(dato => {
+        const div = document.createElement('div')
+        div.innerHTML = "<div>ID: "+ dato.id+" </div>" +
+        "<div> "+dato.author+" </div>"
+        body.appendChild(div)
+    })
+}
+
+/* //Desestructurado
+const mostrarHTML = (respuesta) => {
+    respuesta.map(dato => {
+        const {id, author} = dato
+        const div = document.createElement('div')
+        div.innerHTML = "<div>ID: "+ id +" </div>" +
+        "<div> "+ author +" </div>"
+        body.appendChild(div)
+    })
+}
+ */
+
+/* // con backstick
+const url = 'https://picsum.photos/list'
+const body = document.querySelector("body")
+
+
+
+function mostrarHTML(datos) {
+    datos.map( dato => {
+        const {author, id, post_url} = dato;
+        const html = document.createElement("div")
+        html.innerHTML= `
+        
+            <p> ----------------</p>
+            <p> Autor: ${author}</p>
+            <p> Id: ${id} </p>
+            <a href="${post_url}" target="_blank">ver imagen</a>
+            <p> ----------------</p>
+
+        
+        `;
+        body.innerHTML = html;
+    });
+
+}
+ */
